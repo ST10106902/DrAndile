ec109415e1186c3430c015d9658c07b8ba87b7c6
thomphani netshidzivhe <netshidzethomphani@gmail.com>
2024-11-19 15:33:29 +0200

    made this logout page display a confirmation  message when you want to logout

--- app/src/main/java/com/example/drandile/LogoutPage.kt
+++ app/src/main/java/com/example/drandile/LogoutPage.kt
@@ -1,20 +1,51 @@
 package com.example.drandile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+
+
 class LogoutPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_logout_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val logoutButton: Button = findViewById(R.id.logoutButton)
+        val cancelButton: Button = findViewById(R.id.cancelButton)
+
+        logoutButton.setOnClickListener {
+            showLogoutConfirmationDialog()
         }
+
+        cancelButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun showLogoutConfirmationDialog() {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Logout Confirmation")
+        alertDialog.setMessage("Do you really want to logout?")
+        alertDialog.setPositiveButton("Yes") { _, _ ->
+            performLogout()
+        }
+        alertDialog.setNegativeButton("No") { dialog, _ ->
+            dialog.dismiss()
+        }
+        alertDialog.show()
+    }
+
+    private fun performLogout() {
+        val intent = Intent(this, Login::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
     }
 }
