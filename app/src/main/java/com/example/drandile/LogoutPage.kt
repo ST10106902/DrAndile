fb0bebc4f77f4514ff1078106cda83e263d6fe34
thomphani netshidzivhe <netshidzethomphani@gmail.com>
2024-11-19 14:51:59 +0200

    Added a logout Page

---
+++ app/src/main/java/com/example/drandile/LogoutPage.kt
@@ -0,0 +1,20 @@
+package com.example.drandile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LogoutPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_logout_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
